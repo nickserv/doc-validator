207f8ad0a72ed83cf9ede82c088d35ea97d39688
Nick McCurdy <nick@nickmccurdy.com>
2021-04-20 07:29:30 -0400

    Skip parsing other languages

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import babel from "@babel/core"
 export function parse(markdown) {
   const snippets = marked
     .lexer(markdown)
-    .filter((token) => token.type === "code")
+    .filter((token) => token.type === "code" && token.lang === "js")
     .map((token) => token.text)
   for (const snippet of snippets) {
     babel.parseSync(snippet, { sourceType: "unambiguous" })
--- test.js
+++ test.js
@@ -4,8 +4,18 @@ function js(src) {
   return `\`\`\`js\n${src}\n\`\`\``
 }
 
-test("parse", () => {
-  const src = 'console.log("Hello, world!")'
-  expect(parse(js(src))).toEqual([src])
-  expect(() => parse(js('console.log("Whoops'))).toThrow(SyntaxError)
+describe("parse", () => {
+  test("parses valid JS", () => {
+    const src = 'console.log("Hello, world!")'
+    expect(parse(js(src))).toEqual([src])
+  })
+
+  test("does not parse invalid JS", () => {
+    expect(() => parse(js('console.log("Whoops'))).toThrow(SyntaxError)
+  })
+
+  test("skips parsing other languages", () => {
+    expect(parse("```\n.\n```")).toEqual([])
+    expect(parse("```sh\n.\n```")).toEqual([])
+  })
 })
