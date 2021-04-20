f5adb9cc3543bafadc8dbe9a1b398588dec987cb
Nick McCurdy <nick@nickmccurdy.com>
2021-04-20 07:21:00 -0400

    Use stdin when path is missing

--- cli.js
+++ cli.js
@@ -2,7 +2,15 @@
 import { parse } from "./index.js"
 import fsCallbacks from "fs"
 
+async function getStdin() {
+  let result = ""
+  for await (const chunk of process.stdin) result += chunk
+  return result
+}
+
 const fs = fsCallbacks.promises
 const path = process.argv[2]
-const text = await fs.readFile(path, { encoding: "utf8" })
+const text = path
+  ? await fs.readFile(path, { encoding: "utf8" })
+  : await getStdin()
 parse(text)
